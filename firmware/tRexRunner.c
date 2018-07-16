577dcea6bf4e8820ca1270d4199f6406db079d26
Daniel Knezevic <danielknezevic90@gmail.com>
2018-07-16 22:13:23 +0200

    Adds game objects and animations
    - trex animations (running, jumping, ducking)
    - horizon movement
    - flying pterodactyl
    - cactus obstacles

--- firmware/tRexRunner.c
+++ firmware/tRexRunner.c
@@ -11,7 +11,96 @@
 #include "tRexRunner.h"
 
 static uint8_t frame_buffer[WIDTH * HEIGHT / 8];
-volatile unsigned int global_clock = 0;
+volatile uint16_t global_clock = 0;
+volatile uint16_t test_clock = 0;
+//128x3
+static const __flash uint8_t horizon_line[] =
+{
+    0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x04, 0x04, 0x04, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x01, 0x01, 0x01, 0x01, 0x01, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02,
+    0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x04, 0x04, 0x04, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x01, 0x01, 0x01, 0x01, 0x01, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02
+};
+
+// 14x16px
+static const __flash uint8_t trex_standing_init[] =
+{
+    0xfc, 0xe0, 0xc0, 0xe0, 0xe0, 0xf0, 0xfe, 0xff, 0xfd, 0x5f, 0xd7, 0x17, 0x07, 0x06,
+    0x01, 0x03, 0x07, 0x3f, 0x2f, 0x07, 0x3f, 0x23, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00
+};
+
+// 14x16px
+static const __flash uint8_t trex_running1[] =
+{
+    0xfc, 0xe0, 0xc0, 0xe0, 0xe0, 0xf0, 0xfe, 0xff, 0xfd, 0x5f, 0xd7, 0x17, 0x07, 0x06,
+    0x01, 0x03, 0x07, 0x1f, 0x17, 0x07, 0x3f, 0x23, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00
+};
+
+// 14x16px
+static const __flash uint8_t trex_running2[] =
+{
+    0xfc, 0xe0, 0xc0, 0xe0, 0xe0, 0xf0, 0xfe, 0xff, 0xfd, 0x5f, 0xd7, 0x17, 0x07, 0x06,
+    0x01, 0x03, 0x07, 0x3f, 0x2f, 0x07, 0x0f, 0x0b, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00
+};
+
+// 14x16px
+static const __flash uint8_t trex_dead[] =
+{
+    0xfc, 0xe0, 0xc0, 0xe0, 0xe0, 0xf0, 0xfe, 0xf5, 0xfd, 0x5f, 0xfb, 0x17, 0x07, 0x06,
+    0x01, 0x03, 0x07, 0x3f, 0x2f, 0x07, 0x3f, 0x23, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00
+};
+
+// 22x10px
+static const __flash uint8_t trex_ducking1[] =
+{
+    0x03, 0x06, 0x0e, 0x1c, 0xfc, 0x7c, 0x3e, 0x1e, 0x7e, 0x5e, 0x1e, 0x7e, 0x5c, 0x0c, 0x1e, 0x1d, 0x1f, 0x1f, 0x17, 0x17, 0x07, 0x06,
+    0x00, 0x00, 0x00, 0x00, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+};
+// FIXME 22x10px
+static const __flash uint8_t trex_ducking2[] =
+{
+    0x03, 0x06, 0x0e, 0x1c, 0x7c, 0x5c, 0x3e, 0xfe, 0x7e, 0x7e, 0x1e, 0x7e, 0x5c, 0x0c, 0x1e, 0x1d, 0x1f, 0x1f, 0x17, 0x17, 0x07, 0x06,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+};
+
+//11x10
+static const __flash uint8_t pterodactyl1[] =
+{
+    0x10, 0x18, 0x1c, 0x30, 0x7f, 0xfe, 0xfc, 0xf8, 0xa0, 0xa0, 0x20,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+};
+
+//11x10
+static const __flash uint8_t pterodactyl2[] =
+{
+    0x10, 0x18, 0x1c, 0x3c, 0xf0, 0xf0, 0xf0, 0xf0, 0xa0, 0xa0, 0x20,
+    0x00, 0x00, 0x00, 0x00, 0x03, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00
+};
+
+//5x8
+static const __flash uint8_t cactus1[] =
+{
+    0x1c, 0x10, 0xfe, 0x20, 0x3c
+};
+
+//5x8
+static const __flash uint8_t cactus2[] =
+{
+    0x0c, 0x08, 0xff, 0x10, 0x1c
+};
+
+//6x9
+static const __flash uint8_t cactus3[] =
+{
+    0x0e, 0x08, 0xfe, 0xff, 0x20, 0x38,
+    0x00, 0x00, 0x01, 0x01, 0x00, 0x00
+};
+
+//8x13
+static const __flash uint8_t cactus4[] =
+{
+    0xf0, 0xf8, 0x80, 0xfe, 0xff, 0x80, 0xf0, 0xf8,
+    0x00, 0x01, 0x01, 0x1f, 0x1f, 0x01, 0x01, 0x00
+};
+
 /*
  * Initialize Timer1
  */
@@ -29,11 +118,37 @@ void TIMER_init(void)
 ISR(TIMER1_COMPA_vect, ISR_NOBLOCK)
 {
     global_clock++;
+    test_clock++;
 }
 
 void FB_Clear()
 {
     memset(frame_buffer, 0, sizeof(uint8_t) * (WIDTH * HEIGHT / 8));
+}
+
+void FB_drawImage(int8_t x, int8_t y, const __flash uint8_t* image, uint8_t width, uint8_t height)
+{
+    for(int h = y; h < (y + height); h++)
+    {
+        for(int w = x; w < (x + width); w++)
+        {
+            if((w >= SSD1306_LCDWIDTH) || (h >= SSD1306_LCDHEIGHT)) continue;
+            if( h < 0 || w < 0 ) continue;
+            uint16_t buffer_index = SSD1306_LCDWIDTH * (h / 8) + w;
+            uint8_t image_w = w - x;
+            uint8_t image_h = h - y;
+            uint16_t image_index = width * (image_h / 8) + image_w;
+            if((image[image_index] >> (image_h % 8) & 0x01) == 0x01)
+            {
+                frame_buffer[buffer_index]|=(1 << (h % 8));
+            }
+            /* //only apply "or" to buffer
+             else
+             {
+             frame_buffer[buffer_index]&=~(1 << (h % 8));
+             }*/
+        }
+    }
 }
 
 int main(void)
@@ -43,17 +158,182 @@ int main(void)
     SSD1306_init();
     SSD1306_clear();
 
-    uint8_t i = 0;
+        game_object_t horizon = {
+    WIDTH,
+    HEIGHT - HORIZON_LINE_HEIGHT - 1,
+    HORIZON_LINE_WIDTH,
+    HORIZON_LINE_HEIGHT, horizon_line };
+    float horizon_delta = 0;
+
+    game_object_t trex = {
+            8,
+            HEIGHT - TREX_STANDING_HEIGHT - 1,
+            TREX_DUCKING_WIDTH,
+            TREX_DUCKING_HEIGHT, trex_ducking2
+    };
+    trex_states_t trex_state = DUCKING;
+    uint8_t trex_running_leg = 0;
+    unsigned int running_counter = 0;
+    float trex_y_delta = HEIGHT - TREX_STANDING_HEIGHT - 1;
+    uint8_t jump_max_y_reached = 0;
+
+    game_object_t pterodactyl = {
+            WIDTH, //TODO change me
+            5,
+            PTERODACTYL_WIDTH,
+            PTERODACTYL_HEIGHT,
+            pterodactyl1
+    };
+    uint8_t pterodactyl_wing = 0;
+    float pterodactyl_x_delta = WIDTH;
+    unsigned int flapping_counter = 0;
 
     while (1)
     {
-        if(global_clock >= RENDER_PERIOD)
+        /* TODO HANDLE BUTTONS */
+        if (test_clock >= 3000)
+        {
+            if (trex_state != JUMPING)
+            {
+                test_clock = 0;
+                jump_max_y_reached = 0;
+                trex_state = JUMPING;
+            }
+        }
+
+        /* UPDATE GAME */
+        if (global_clock >= RENDER_PERIOD)
         {
             global_clock = 0;
             FB_Clear();
-            memset(frame_buffer, ++i, sizeof(uint8_t) * (WIDTH * HEIGHT / 8));
+
+
+            /* HORIZONT LINE BEGIN*/
+            FB_drawImage(horizon.x, horizon.y, horizon.sprite,
+                    horizon.width, horizon.height);
+            if (horizon_delta < 0)
+            {
+                FB_drawImage(WIDTH + horizon.x, horizon.y,
+                        horizon.sprite, horizon.width, horizon.height);
+            }
+            if (horizon_delta + 128 > 0)
+            {
+                horizon_delta -= GAME_SPEED;
+            }
+            else
+            {
+                horizon_delta = 0;
+            }
+            horizon.x = floor(horizon_delta);
+            /* HORIZONT LINE END*/
+
+            FB_drawImage(30, 22, cactus1, 5, 8);
+            FB_drawImage(36, 22, cactus2, 5, 8);
+            FB_drawImage(70, 21, cactus3, 6, 9);
+            FB_drawImage(100, 17, cactus4, 8, 13);
+
+            /* PTERODACTYL BEGIN */
+            if (pterodactyl_x_delta + PTERODACTYL_WIDTH > 0)
+                pterodactyl_x_delta -= GAME_SPEED;
+            else
+                pterodactyl_x_delta = WIDTH; //TODO change to random walue
+            pterodactyl.x = floor(pterodactyl_x_delta);
+
+            if (++flapping_counter >= PTERODACTYL_WING_SWAP)
+            {
+
+                flapping_counter = 0;
+
+                if (pterodactyl_wing)
+                {
+                    pterodactyl.sprite = pterodactyl1;
+                    pterodactyl_wing = 0;
+                } else
+                {
+                    pterodactyl.sprite = pterodactyl2;
+                    pterodactyl_wing = 1;
+                }
+            }
+
+            FB_drawImage(pterodactyl.x, pterodactyl.y, pterodactyl.sprite,
+                        pterodactyl.width, pterodactyl.height);
+            /* PTERODACTYL END */
+
+            if (trex_state == RUNNING)
+            {
+                trex.x = 8; //FIXME
+                trex.y = HEIGHT - TREX_STANDING_HEIGHT - 1;
+                trex.width = TREX_STANDING_WIDTH;
+                trex.height = TREX_STANDING_HEIGHT;
+                if (++running_counter >= TREX_RUNNING_SPEED)
+                {
+                    running_counter = 0;
+                    if (trex_running_leg)
+                    {
+                        trex_running_leg = 0;
+                        trex.sprite = trex_running2;
+                    } else
+                    {
+                        trex_running_leg = 1;
+                        trex.sprite = trex_running1;
+                    }
+                }
+                FB_drawImage(trex.x, trex.y, trex.sprite, trex.width,
+                        trex.height);
+            } else if (trex_state == DUCKING)
+            {
+                trex.x = 8; //FIXME
+                trex.y = HEIGHT - TREX_DUCKING_HEIGHT - 1;
+                trex.width = TREX_DUCKING_WIDTH;
+                trex.height = TREX_DUCKING_HEIGHT;
+                if (++running_counter >= TREX_RUNNING_SPEED)
+                {
+                    running_counter = 0;
+                    if (trex_running_leg)
+                    {
+                        trex_running_leg = 0;
+                        trex.sprite = trex_ducking1;
+                    } else
+                    {
+                        trex_running_leg = 1;
+                        trex.sprite = trex_ducking2;
+                    }
+                }
+                FB_drawImage(trex.x, trex.y, trex.sprite, trex.width,
+                        trex.height);
+            } else if (trex_state == JUMPING)
+            {
+                trex.width = TREX_STANDING_WIDTH;
+                trex.height = TREX_STANDING_HEIGHT;
+                trex.sprite = trex_standing_init;
+
+                /* jump up */
+                if (!jump_max_y_reached
+                        && trex.y >= (HEIGHT - TREX_MAX_JUMP_HEIGHT))
+                {
+                    trex_y_delta -= JUMPING_SPEED;
+                    trex.y = floor(trex_y_delta);
+                } else
+                    jump_max_y_reached = 1;
+
+                /* let gravity do the landing */
+                if (jump_max_y_reached
+                        && trex.y <= (HEIGHT - TREX_STANDING_HEIGHT - 2))
+                {
+                    trex_y_delta += GAME_GRAVITY;
+                    trex.y = floor(trex_y_delta);
+                }
+
+                //next state running
+                if (jump_max_y_reached
+                        && trex.y > (HEIGHT - TREX_STANDING_HEIGHT - 2))
+                    trex_state = RUNNING;
+
+                FB_drawImage(trex.x, trex.y, trex.sprite, trex.width,
+                        trex.height);
+            }
+            /* RENDER */
             SSD1306_display(frame_buffer);
         }
     }
 }
-
--- firmware/tRexRunner.h
+++ firmware/tRexRunner.h
@@ -13,7 +13,39 @@
 #define WIDTH                   SSD1306_LCDWIDTH
 #define HEIGHT                  SSD1306_LCDHEIGHT
 
-#define RENDER_PERIOD           1000
+#define RENDER_PERIOD           10 // 100 FPS
+
+#define HORIZON_LINE_WIDTH      WIDTH
+#define HORIZON_LINE_HEIGHT     3
+
+#define TREX_STANDING_WIDTH     14
+#define TREX_STANDING_HEIGHT    14
+
+#define TREX_DUCKING_WIDTH      22
+#define TREX_DUCKING_HEIGHT     9
+
+#define PTERODACTYL_WIDTH       11
+#define PTERODACTYL_HEIGHT      10
+#define PTERODACTYL_WING_SWAP   20
+
+#define TREX_RUNNING_SPEED      10
+#define TREX_MAX_JUMP_HEIGHT    (HEIGHT - 2)
+
+#define GAME_GRAVITY            0.65f
+#define GAME_SPEED              0.4f
+#define JUMPING_SPEED           0.55f
+
+typedef enum trex_states_e {
+    CRASHED = 0, DUCKING, JUMPING, RUNNING, WAITING
+} trex_states_t;
+
+typedef struct game_object_s {
+    uint8_t x;
+    uint8_t y;
+    uint8_t width;
+    uint8_t height;
+    uint8_t *sprite;
+} game_object_t;
 
 void FB_Clear();
 
